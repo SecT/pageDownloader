6ff4da8e3388ae42d2a28ffcd403cbc24450f48c
SecretT <tajnyt@gmail.com>
2018-07-22 08:44:02 +0200

    ContentDownloadHelper: replacing illegal file name characters

--- contentDownloadHelper.py
+++ contentDownloadHelper.py
@@ -6,6 +6,11 @@ class ContentDownloadHelper:
         pass
 
     def saveContentToFile( filename, content, format='htm', fileEncoding='utf-8'):
+
+        filename = filename.replace(':','-')
+        filename = filename.replace('<', '_')
+        filename = filename.replace('>', '_')
+
         newFile = open(filename+'.'+format, 'w', encoding=fileEncoding)
         newFile.write(content)
         newFile.close()
