b79aaeacc988e633724292314712efe3d28d93c3
SecT <tajnyt@gmail.com>
2021-05-08 00:14:08 +0200

    Add user agent to urlopen

--- contentDownloadHelper.py
+++ contentDownloadHelper.py
@@ -1,14 +1,17 @@
-from urllib.request import urlretrieve
-from urllib.request import urlopen
+#from urllib.request import urlretrieve
+from urllib.request import Request, urlopen
+from shutil import copyfileobj
 
 class ContentDownloadHelper:
     def __init__(self):
         pass
 
     def getPageContents(url, charset):
-        page = urlopen(url)
+
+        req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req)
         return page.read().decode(charset, 'ignore')
-		
+
     def saveContentToFile( filename, content, targetDir, format='htm', fileEncoding='utf-8'):
 
         filename = filename.replace(':','-')
@@ -22,5 +25,11 @@ class ContentDownloadHelper:
             print(e)
 
     def saveImg( url, filename, targetDir):
-        urlretrieve(url, targetDir+filename)
+        #urlretrieve(url, targetDir+filename)  #urlretrieve is deprecated, see python docs
+
+        print('saveImg: '+url)
+        req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+
+        with urlopen(req) as in_stream, open(targetDir+filename, 'wb') as out_file:
+            copyfileobj(in_stream, out_file)
 
